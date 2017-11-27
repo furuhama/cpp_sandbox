6b9ee66fd0f4d7b880e9d4a7ceea32521c363d1a
furuhama <furuhama@users.noreply.github.com>
2017-11-27 14:00:48 +0900

    enable cpplint

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-using namespace std;
+namespace std {
 
-int main()
-{
-    int a;
-    cout << "hello, world!" << endl;
-    cout << "please input integer, 5 times" << endl;
-    int total;
-    for (int i = 0; i < 5; i++)
-    {
-        cin >> a;
-        total += a;
-    }
-    cout << "computing";
-    cout << "sum of numbers you input: " << total << endl;
-    return 0;
+int main() {
+  int a;
+  cout << "hello, world!" << endl;
+  cout << "please input integer, 5 times" << endl;
+  int total;
+  for (int i = 0; i < 5; i++) {
+    cin >> a;
+    total += a;
+  }
+  cout << "computing";
+  cout << "sum of numbers you input: " << total << endl;
+  return 0;
 }
+
+}  // namespace std
