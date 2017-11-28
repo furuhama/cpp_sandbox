84b93fd4b3b05443f379a30f322a21e334e5bf70
furuhama <furuhama@users.noreply.github.com>
2017-11-28 11:09:09 +0900

    set Monster class and roar method in hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,14 +1,31 @@
 /* Copyright 2017 furuhama */
 #include <iostream>
+#include <string>
 
 int main() {
   using std::cout;
   using std::cin;
   using std::endl;
+  using std::string;
 
-  char name;
+  class Monster {
+    private:
+      string name;
+    public:
+      Monster(string n) {
+        name = n;
+      };
+      void roar() {
+        cout << "grrrr... my name is " << name << endl;
+      };
+  };
+
+  string name;
   cout << "please input name of monster" << endl;
   cin >> name;
-  cout << "your monster name: " << name << endl;
+  cout << "summon your monster..." << endl;
+  // set Monster instance
+  Monster m(name);
+  m.roar();
   return 0;
 }
