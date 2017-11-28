02a459855e5b65e179a8422114157620e9843786
furuhama <furuhama@users.noreply.github.com>
2017-11-28 10:53:15 +0900

    edit hello.cpp to input monster name

--- hello.cpp
+++ hello.cpp
@@ -6,15 +6,9 @@ int main() {
   using std::cin;
   using std::endl;
 
-  int a;
-  cout << "hello, world!" << endl;
-  cout << "please input integer, 5 times" << endl;
-  int total;
-  for (int i = 0; i < 5; i++) {
-    cin >> a;
-    total += a;
-  }
-  cout << "computing";
-  cout << "sum of numbers you input: " << total << endl;
+  char name;
+  cout << "please input name of monster" << endl;
+  cin >> name;
+  cout << "your monster name: " << name << endl;
   return 0;
 }
