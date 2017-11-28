49fa9b170465e469e5431be004cf27adad22fb58
furuhama <furuhama@users.noreply.github.com>
2017-11-28 12:07:47 +0900

    fix indent in class definition & not to use std::cin

--- hello.cpp
+++ hello.cpp
@@ -9,20 +9,21 @@ int main() {
   using std::string;
 
   class Monster {
-    private:
-      string name;
-    public:
-      Monster(string n) {
-        name = n;
-      };
-      void roar() {
-        cout << "grrrr... my name is " << name << endl;
-      };
+   private:
+     string name;
+   public:
+     explicit Monster(string n) {
+       name = n;
+     }
+     void roar() {
+       cout << "grrrr... my name is " << name << endl;
+     }
   };
 
   string name;
   cout << "please input name of monster" << endl;
-  cin >> name;
+  // cin >> name;
+  name = "tama";
   cout << "summon your monster..." << endl;
   // set Monster instance
   Monster m(name);
