3a2d996f32fe5ca7ac9d7405bc82a48d64623622
furuhama <furuhama@users.noreply.github.com>
2017-11-28 14:33:43 +0900

    make iterationTest.cpp

---
+++ iterationTest.cpp
@@ -0,0 +1,11 @@
+/* Copyright 2017 furuhama */
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        int a = i + 1;
+        int b = i * 2;
+        std::cout << a + b << std::endl;
+    }
+    return 0;
+}
