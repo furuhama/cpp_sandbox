9d66f4c5c8e0128129a83a396735fa4a0775931f
furuhama <furuhama@users.noreply.github.com>
2018-03-11 16:45:08 +0900

    Make char.c

---
+++ src/c/chars.c
@@ -0,0 +1,11 @@
+/* Copyright 2018 furuhama */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[] = "145";
+    int suuti = atoi(str);
+    printf("%d\n",suuti);
+    return 0;
+}
