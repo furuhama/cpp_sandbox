0638fdedcaecf9cf6a142c5c2e9add9bebeafaeb
furuhama <furuhama@users.noreply.github.com>
2018-03-12 01:31:36 +0900

    Update char.c

--- src/c/chars.c
+++ src/c/chars.c
@@ -4,8 +4,16 @@
 #include <stdlib.h>
 
 int main() {
-    char str[] = "145";
-    int suuti = atoi(str);
+    // try to print char array without EOS(= End Of String)
+    char str1[2] = {'a', 'b'};
+    int i = strlen(str1);
+    // Type casting
+    char str2[] = "145";
+    int suuti = atoi(str2);
+
+    printf("%s\n", str1);
+    printf("%d\n", i);
     printf("%d\n",suuti);
+
     return 0;
 }
