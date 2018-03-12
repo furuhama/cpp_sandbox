91f2f29a65ac8530c28e3da53c85389b97d4a133
furuhama <furuhama@users.noreply.github.com>
2018-03-12 22:31:07 +0900

    Edit type & Declare including to remove warning

--- src/c/chars.c
+++ src/c/chars.c
@@ -2,12 +2,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "debug_macro.h"
 
 int main() {
     // try to print char array without EOS(= End Of String)
     char str1[2] = {'a', 'b'};
-    int i = strlen(str1);
+    uint32_t i = strlen(str1);
     // Type casting
     char str2[] = "145";
     int num = atoi(str2);
