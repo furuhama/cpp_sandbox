2a533808b7aea19111962bfc33c1e6f147d7345a
furuhama <furuhama@users.noreply.github.com>
2018-03-12 09:19:15 +0900

    Define debug macro, Rename var

--- src/c/chars.c
+++ src/c/chars.c
@@ -3,17 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DEBUG_S(x) printf(#x "\t= %s\n", (x))
+#define DEBUG_D(x) printf(#x "\t= %d\n", (x))
+
 int main() {
     // try to print char array without EOS(= End Of String)
     char str1[2] = {'a', 'b'};
     int i = strlen(str1);
     // Type casting
     char str2[] = "145";
-    int suuti = atoi(str2);
+    int num = atoi(str2);
 
     printf("%s\n", str1);
     printf("%d\n", i);
-    printf("%d\n",suuti);
+    printf("%d\n",num);
+    DEBUG_S(str1);
+    DEBUG_D(i);
+    DEBUG_D(num);
 
     return 0;
 }
