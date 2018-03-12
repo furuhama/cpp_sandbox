6a6fa1bd80d740bbab24fd873686bf067a3d03e3
furuhama <furuhama@users.noreply.github.com>
2018-03-12 22:25:06 +0900

    Use #pragma once

--- src/c/debug_macro.h
+++ src/c/debug_macro.h
@@ -1,8 +1,5 @@
 /* Copyright 2018 furuhama */
 
-#ifndef _SRC_C_DEBUG_MACRO_H_
-#define _SRC_C_DEBUG_MACRO_H_
+#pragma once
 
 #define DEBUG(type, var) printf(#var "\t= %" #type "\n", (var))
-
-#endif  // _SRC_C_DEBUG_MACRO_H_
