b8d200a455efd78be211d2c075f1f6dc8bf3a773
furuhama <furuhama@users.noreply.github.com>
2018-03-17 15:51:21 +0900

    Add function using pointer

--- src/c/pointer.c
+++ src/c/pointer.c
@@ -1,6 +1,9 @@
 /* Copyright 2018 furuhama */
 
+#include <stdio.h>
 #include "./debug_macro.h"
+
+void func(int *pvalue);  // prototype definition
 
 int main() {
     int *p;
@@ -12,5 +15,20 @@ int main() {
     DEBUG(p, &i);
     DEBUG(d, i);
 
+    // use pointer in function
+
+    int value = 5;
+
+    DEBUG(p, &value);
+    DEBUG(d, value);
+    func(&value);
+    DEBUG(d, value);
+
     return 0;
+}
+
+void func(int *pvalue) {
+    DEBUG(p, pvalue);
+    *pvalue = 100;
+    return;
 }
