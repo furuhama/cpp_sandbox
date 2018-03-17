864057dda94f0ed1a33e30194cc4164aa46c7f83
furuhama <furuhama@users.noreply.github.com>
2018-03-17 16:53:40 +0900

    Add enum

---
+++ src/c/enum.c
@@ -0,0 +1,23 @@
+/* Copyright 2018 furuhama */
+
+#include "./debug_macro.h"
+
+enum {
+    E_0,
+    E_1,
+    E_5 = 5,
+    E_6,
+    E_7,
+    E_9 = 9,
+};
+
+int main() {
+    DEBUG(d, E_0);
+    DEBUG(d, E_1);
+    DEBUG(d, E_5);
+    DEBUG(d, E_6);
+    DEBUG(d, E_7);
+    DEBUG(d, E_9);
+
+    return 0;
+}
