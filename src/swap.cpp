946703eae644da977be347109712f743cc3db017
furuhama <furuhama@users.noreply.github.com>
2018-05-25 00:17:44 +0900

    Make swapping by using references

---
+++ src/swap.cpp
@@ -0,0 +1,20 @@
+/* Copyright 2017 furuhama */
+
+#include <iostream>
+
+void swap(int& n1, int& n2) {
+    int tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+}
+
+int main() {
+    int n1 = 3652;
+    int n2 = 82735;
+
+    std::cout << n1 << ", " << n2 << std::endl;
+    swap(n1, n2);
+    std::cout << n1 << ", " << n2 << std::endl;
+
+    return 0;
+}
