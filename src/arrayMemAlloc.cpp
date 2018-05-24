8ab8f9a93298120b98dfbd5aacf6573251b016af
furuhama <furuhama@users.noreply.github.com>
2018-05-25 00:05:03 +0900

    Make Array Memory Allcation

---
+++ src/arrayMemAlloc.cpp
@@ -0,0 +1,17 @@
+/* Copyright 2017 furuhama */
+
+#include <iostream>
+
+int main() {
+    int* p = new int[100];
+
+    for (int i = 0; i < 100; i++) {
+        p[i] = i * i;
+        std::cout << p[i] << std::endl;
+    }
+
+    // Array Memory free should be `delete[]` (NOT `delete` )
+    delete[] p;
+
+    return 0;
+}
