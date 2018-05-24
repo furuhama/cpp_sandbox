f91e145aaeeb971587b95ece9fce2898e0284623
furuhama <furuhama@users.noreply.github.com>
2018-05-24 21:46:05 +0900

    Make Rectangle class

---
+++ src/rectClass.cpp
@@ -0,0 +1,34 @@
+/* Copyright 2017 furuhama */
+
+// class definition
+
+#include <iostream>
+
+class Rectangle {
+    int width, height;
+ public:
+    void set_values(int x, int y);
+    int get_area();
+};
+
+void Rectangle::set_values(int x, int y) {
+    width = x;
+    height = y;
+}
+
+int Rectangle::get_area() {
+    return width * height;
+}
+
+int main() {
+    // make Rectangle class instance
+    Rectangle rect1, rect2;
+
+    rect1.set_values(1, 2);
+    rect2.set_values(3, 4);
+
+    std::cout << "rect1's area: " << rect1.get_area() << std::endl;
+    std::cout << "rect2's area: " << rect2.get_area() << std::endl;
+
+    return 0;
+}
